b1da876c5c379b3001c731c65e978752f388d7d9
James <james.jiang13@gmail.com>
2020-12-29 13:36:52 -0800

    Added reveal.js and intial slides

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello webpack';
-  return element;
-}
+// function component() {
+//   const element = document.createElement('div');
+//   element.innerHTML = 'Hello webpack';
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+let deck = new Reveal({
+  plugins: [Markdown],
+})
+
+deck.initialize();
