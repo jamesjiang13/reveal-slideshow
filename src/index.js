d576b2e1ed994bb7fe16e6ad50a66f7edd908b67
James <james.jiang13@gmail.com>
2020-12-30 14:27:29 -0800

    Fixed css for opening/closing

--- src/index.js
+++ src/index.js
@@ -4,23 +4,22 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 window.onload = () => {
   
   function showExit() {
-    var element = document.getElementById("exit");
+    let element = document.getElementById("exit");
     element.classList = "";
     element.classList.add("show-exit");
   }
 
   function hideExit() {
-    var element = document.getElementById("exit");
+    let element = document.getElementById("exit");
     element.classList = "";
     element.classList.add("hide-exit");
   }
 
-  document.getElementById("start")
+  document.getElementById("start-btn")
           .addEventListener("click", () => {
 
-            document.getElementById("start").classList.add("hide")
-            document.getElementById("reveal")
-                    .classList.remove("hide")
+            document.getElementById("start-container").classList.add("hide")
+            document.getElementById("reveal").classList.remove("hide")
 
             let deck = new Reveal({
               plugins: [Markdown],
@@ -32,9 +31,10 @@ window.onload = () => {
 
   document.getElementById("exit")
           .addEventListener("click", () => {
-            
+
             document.getElementById("reveal").classList.add("hide");
-            document.getElementById("start").classList.remove("hide")
+            document.getElementById("start-container").classList.remove("hide")
+            document.getElementById("body").classList.remove("reveal-viewport")
             
             hideExit();
           });
